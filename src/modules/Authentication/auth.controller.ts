6075407339dde6e23d61437682b542b3bbd29550
sakib404-hub <akibhossainsakib1040@gmail.com>
2026-07-15 00:50:42 +0600

    setting up the access token and the refresh token in te cookie

--- src/modules/Authentication/auth.controller.ts
+++ src/modules/Authentication/auth.controller.ts
@@ -26,6 +26,21 @@ const loginUser = catchAsync(async(req : Request, res : Response, next : NextFun
 
     const {accessToken, refreshToken} = await authServices.loginUser(payLoad);
 
+    //? setting the access and the refresh token in the browser cookie
+    res.cookie("accessToken",accessToken, {
+        httpOnly : true,
+        secure : false,
+        sameSite : "none",
+        maxAge : 1000 * 60 * 60 * 24 //? 1day maximum age
+    })
+
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly : true,
+        secure : false,
+        sameSite : "none",
+        maxAge : 1000 * 60 * 60 * 24 * 7
+    })
+
     return sendResponse(res, {
         success : true,
         statusCode : status.OK,
