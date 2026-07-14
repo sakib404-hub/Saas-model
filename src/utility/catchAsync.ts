3e368008ff11a9f8214708f1ffec111684f9f13e
sakib404-hub <akibhossainsakib1040@gmail.com>
2026-07-14 22:48:15 +0600

    added catchAsync for the request response try catch

---
+++ src/utility/catchAsync.ts
@@ -0,0 +1,15 @@
+import type { NextFunction, Request, Response } from "express"
+
+const catchAsync = async(fn : Function)=>{
+    return async(req : Request, res : Response, next : NextFunction)=>{
+        try{
+            await fn(req, res, next)
+
+        }catch(err){
+
+            next(err);
+        }
+    }
+}
+
+export default catchAsync;
